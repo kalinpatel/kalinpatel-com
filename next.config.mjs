c0e634bac066cff09d01d165cf482721b27030b2
Kalin Patel <52113248+kalinpatel@users.noreply.github.com>
2025-04-24 23:48:37 -0500

    Chunk webpack on builds

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,14 @@ const nextConfig = {
       },
     ];
   },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.optimization.splitChunks = {
+        chunks: "all",
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
